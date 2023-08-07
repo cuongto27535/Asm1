bd1fd18859fbcf21665525e22f0ea5ff8f3edf96
cuongto27535 <117632972+cuongto27535@users.noreply.github.com>
2023-08-08 00:07:51 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const ProductModel = require('./model');
+const uri = "mongodb+srv://ToLeCuong:cuong2k32010@cluster0.t4dvo7u.mongodb.net/asmnetworking?retryWrites=true&w=majority";
+
+mongoose.connect(uri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}) 
+.then(() => {
+    console.log('Da ket noi voi MongoDB');
+})
+.catch((err) => {
+    console.error('Khong ket noi dc MongoDB: ', err);
+})
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/list', async function (req, res) {
+
+    try {
+        const productList = await ProductModel.find().lean();
+        res.json(productList);
+      } catch (err) {
+        console.error('Error fetching products:', err);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+})
+
+// Định nghĩa API thêm sản phẩm
+app.post('/addProduct', async (req, res) => {
+    try {
+      const { name, price, description, image} = req.body;
+      const product = new ProductModel({ name, price, description, image });
+      await product.save();
+      showList(res);
+    } catch (err) {
+      console.error('Error adding product:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  // Định nghĩa API sửa đổi sản phẩm
+  app.put('/product/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, price, description, image } = req.body;
+      const product = await ProductModel.findByIdAndUpdate(
+        id,
+        { name, price, description, image },
+        { new: true }
+      );
+      showList(res);
+    } catch (err) {
+      console.error('Error updating product:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  // Định nghĩa API xóa sản phẩm
+  app.delete('/product/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      await ProductModel.findByIdAndRemove(id);
+      // res.json({ success: true });
+      showList(res);
+    } catch (err) {
+      console.error('Error deleting product:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  async function showList(res){
+    const productList = await ProductModel.find().lean();
+    res.json(productList);
+  }
+  
+  // Khởi động server
+  const port = 8000;
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
